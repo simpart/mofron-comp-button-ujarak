d417e27976e4f13c7c8ddbf02bf88154ecd44ef9
simpart <simpart>
2020-08-06 04:20:33 +0000

    refactoring

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-# mofron-comp-ujarakbtn
-this is expansion button component for mofron.<br>
-please see [here](https://github.com/mofron/mofron) about an overview of mofron
+# mofron-comp-button-ujarak
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+ujarak button component for mofron
+
+this source based on the codrops: https://github.com/codrops/ButtonStylesInspiration
+
+## Attention
+ - this component doesn't support baseColor function.
 
 # Install
-
-```bash
-npm install mofron-comp-ujarakbtn
+```
+npm install mofron mofron-comp-button-ujarak
 ```
 
-# Quick Start
+# Parameter
 
-```javascript
-require('mofron');
-let Button = require('mofron-comp-ujarakbtn');
-new Button('Ujarak Button').visible(true);
-```
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+| | text | mixed | string: button text contents |
+| | | | mofron-comp-text: button text component |
+| | | | undefined: call as getter |
+| | mainColor | mixed (color) | string: button text color name, #hex |
+| | | | array: [red, green, blue, (alpha)] |
+| | | | undefined: calla as getter |
+| | | dict | style option |
+| | accentColor | mixed (color) | string: hover color name, #hex |
+| | | | array: [red, green, blue, (alpha)] |
+| | | | undefined: calla as getter |
 
-# Class Specification
-please see [button component spacification](https://github.com/mofron/mofron-comp-button)
--- index.js
+++ index.js
@@ -1,82 +1,126 @@
 /**
- * @file   mofron-comp-button-ujarak/index.js
- * @author simpart
+ * @file mofron-comp-button-ujarak/index.js
+ * @brief ujarak button component for mofron
+ *        this source based on the codrops: https://github.com/codrops/ButtonStylesInspiration
+ * @attention this component doesn't support baseColor function.
+ * @license MIT
  */
-const mf     = require('mofron');
-const Button = require('mofron-comp-button');
-const Hover  = require('mofron-event-hover');
-const Fade   = require('mofron-effect-fade');
-const Scale  = require('mofron-effect-scale');
-const Style  = require('mofron-effect-style');
-const Border = require('mofron-effect-border');
+const Button  = require('mofron-comp-button');
+const Hover   = require('mofron-event-hover');
+const Fade    = require('mofron-effect-fade');
+const Scale   = require('mofron-effect-scale');
+const Style   = require('mofron-effect-style');
+const Border  = require('mofron-effect-border');
+const comutl  = mofron.util.common;
+const ConfArg = mofron.class.ConfArg;
 
-/**
- * @class mofron.comp.button.Ujarak
- * @brief ujarak button class
- */
-mofron.comp.UjarakBtn = class extends Button {
-    
+module.exports = class extends Button {
     /**
      * initialize component
      *
-     * @param prm (string,option) : button contents
-     * @param opt (array) : option
+     * @param (mixed) string: button text contents
+     *                    dict: button component config
+     * @type private
      */
-    constructor (po) {
+    constructor (p1) {
         try {
             super();
             this.name('UjarakBtn');
-            this.prmOpt(po);
+            
+	    if (0 < arguments.length) {
+                this.config(p1);
+            }
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    initDomConts(prm) {
+    /**
+     * initialize dom contens
+     * 
+     * @type private
+     */
+    initDomConts() {
         try {
-            super.initDomConts(prm);
+            super.initDomConts();
 
             this.style({
                 'background': 'none',        // disabled button style
-                'position'  : 'relative'
+                'position'  : 'relative',
+		'-webkit-transition-timing-function': 'cubic-bezier(0.2, 1, 0.3, 1)',
+		'transition-timing-function': 'cubic-bezier(0.2, 1, 0.3, 1)'
+   
             });
-            this.child(this.band());
             
-            let hvr_fnc = (cmp, flg) => {
+	    let bef = {
+                "-webkit-transition-timing-function": "cubic-bezier(0.2, 1, 0.3, 1)",
+		"transition-timing-function": "cubic-bezier(0.2, 1, 0.3, 1)",
+                "content": '""',
+	        "position": "absolute",
+                "top": "0",
+                "left": "0",
+                "width": "100%",
+                "height": "100%",
+                "background": "#37474f",
+                "z-index": "-1",
+                "opacity": "0",
+                "-webkit-transform": "scale3d(0.7, 1, 1)",
+                "transform": "scale3d(0.7, 1, 1)",
+                "-webkit-transition": "-webkit-transform 0.4s, opacity 0.4s",
+                "transition": "transform 0.4s, opacity 0.4s",
+                "-webkit-transition-timing-function": "cubic-bezier(0.2, 1, 0.3, 1)",
+                "transition-timing-function": "cubic-bezier(0.2, 1, 0.3, 1)"
+	    };
+	    let hovbef = {
+                "opacity": "1",
+                "-webkit-transform": "translate3d(0, 0, 0)",
+                "transform": "translate3d(0, 0, 0)"
+	    }
+
+            let addstyle = comutl.obj2style("#" + this.rootDom()[0].id() + "::before", bef) + "\n";
+	    addstyle += comutl.obj2style("#" + this.rootDom()[0].id() + ":hover::before", hovbef) + "\n";
+	    comutl.addhead("style", {id: this.id() + "_style"}, addstyle);
+            
+	    let ujrk = this;
+            let invert = (h1,h2) => {
                 try {
-                    cmp.execEffect((true === flg) ? 2 : 3);
-                    cmp.band().visible(flg);
-                } catch (e) {
+		    let eid = null;
+		    if (true === h2) {
+                        eid = 2;
+		    } else {
+		        eid = 3;
+                    }
+		    ujrk.text().execEffect(eid);
+		} catch (e) {
                     console.error(e.stack);
                     throw e;
-                }
-            }
-            this.event([ new Hover(hvr_fnc) ]);
-            this.effect([
-                new Style({
-                    style: { 'background': 'none' }, speed: 150, eid: 2,
-                    tag: this.name() + 'hover-true'
-                }),
-                new Style({
-                    style: { 'background': 'none' }, speed: 150, eid: 3,
-                    tag: this.name() + 'hover-false'
-                }),
-                new Border({ color: [120,120,120] })
+		}
+	    }
+	    this.text().effect([
+	        new Style({ "style" : { "color" : "rgb(255,255,255)" }, eid:2, speed:400 }),
+		new Style({ "style" : { "color" : null }, eid:3, speed:400 }),
             ]);
+	    this.text().event(new Hover(invert));
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * button text contents setter/getter
+     * 
+     * @param (mixed) string: button text contents
+     *                mofron-comp-text: button text component
+     *                undefined: call as getter
+     * @return (mofron-comp-text) button text contents 
+     * @type parameter
+     */
     text (prm) {
         try {
-            if (true === mf.func.isInclude(prm, "Text")) {
-                prm.style({
-                    'position' : 'relative',
-                    'z-index'  : '1'
-                });
+            if (true === comutl.isinc(prm, "Text")) {
+                prm.style({ 'position': 'relative', 'z-index': '1' });
             }
             return super.text(prm);
         } catch (e) {
@@ -85,56 +129,56 @@ mofron.comp.UjarakBtn = class extends Button {
         }
     }
     
-    band (prm) {
+    /**
+     * button text color setter/getter
+     * 
+     * @param (mixed (color)) string: button text color name, #hex
+     *                        array: [red, green, blue, (alpha)]
+     *                        undefined: calla as getter
+     * @param (dict) style option
+     * @return (mixed) button text color
+     *                 null: not set yet
+     * @type parameter
+     */
+    mainColor (prm,opt) {
         try {
-            if (undefined !== prm) {
-                prm.option({
-                    visible: false, size: ['100%', '100%'],
-                    style: {
-                        'position': 'absolute',
-                        'top'     : '0px',
-                        'left'    : '0px',
-                    },
-                    effect: [
-                        new Fade({ value: true, speed: 100 }),
-                        new Fade({ value: false, speed: 100, eid: 1 }),
-                        new Scale({ value: [1, 1, 1],   eid: 0 }),
-                        new Scale({ value: [0.7, 1, 1], eid: 1 })
-                    ]
-                });
-            }
-            return this.innerComp('band', prm, mf.Component);
+	    this.text().effect({ name: "Style", eid: 3 }).style({
+	        "color" : comutl.getcolor(prm).toString()
+            });
+	    return this.text().mainColor(prm,opt);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
-    
-    mainColor (prm) {
+
+    baseColor (prm) {
         try {
-            let ret = this.tgtColor('background', prm);
-            if (undefined !== prm) {
-                this.effect(['Style', this.name() + 'hover-true']).style({
-                    'background' : this.tgtColor('background')
-                });
-            }
-            return ret;
-        } catch (e) {
+	} catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * hover base color setter/getter
+     * 
+     * @param (mixed (color)) string: hover color name, #hex
+     *                        array: [red, green, blue, (alpha)]
+     *                        undefined: calla as getter
+     * @return (mixed) hover color
+     *                 null: not set yet
+     * @type parameter
+     */
     accentColor (prm) {
         try {
-            if (undefined === prm) {
-                return this.band().baseColor();
-            }
-            this.band().option({ baseColor: prm });
+	    let bef = { "background" : prm };
+            let addstyle = comutl.obj2style("#" + this.rootDom()[0].id() + "::before", bef) + "\n";
+            comutl.addhead("style", {id: this.id() + "_basecolor"}, addstyle);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mofron.comp.UjarakBtn;
+/* end of file */
